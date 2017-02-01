e207103953f0e95d9ef21b42d3d8ba4896efd860
Sergey Chikuyonok <serge.che@gmail.com>
2017-02-01 12:03:17 +0300

    Initial prototype

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import autocomplete from './lib/autocomplete';
+
+export function getAutocomplete() {
+	return autocomplete;
+}
---
+++ rollup.config.js
@@ -0,0 +1,6 @@
+export default {
+	entry: './index.js',
+	external: ['@emmetio/expand-abbreviation'],
+	format: 'cjs',
+	dest: 'dist/atom-plugin.js'
+};
