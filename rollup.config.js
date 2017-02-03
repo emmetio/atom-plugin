3bfc1a353afc4821a6b9c928a77f1020454d4444
Sergey Chikuyonok <serge.che@gmail.com>
2017-02-04 02:17:52 +0300

    Initial implementation of Emmet-as-autocomplete

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 export default {
 	entry: './index.js',
-	external: ['@emmetio/expand-abbreviation'],
+	external: ['@emmetio/expand-abbreviation', '@emmetio/extract-abbreviation'],
 	format: 'cjs',
 	dest: 'dist/atom-plugin.js'
 };
