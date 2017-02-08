ecd43b9c24f05d908a56720e9aa797c83bab935d
Sergey Chikuyonok <serge.che@gmail.com>
2017-02-08 13:06:03 +0300

    Mark Emmet abbreviation in text editor

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 'use strict';
 
 import autocomplete from './lib/autocomplete';
+import AbbreviationMarker from './lib/abbreviation-marker';
+
+let marker;
 
 export function getAutocomplete() {
 	return autocomplete('.text.html');
+}
+
+export function activate() {
+	marker = new AbbreviationMarker();
+}
+
+export function deactivate() {
+	if (marker) {
+		marker.dispose();
+		marker = null;
+	}
 }
