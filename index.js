6e740bc811951a2b532c7656b160a486bf3c4839
Sergey Chikuyonok <serge.che@gmail.com>
2017-02-22 03:20:21 +0300

    Insert formatted line break

--- index.js
+++ index.js
@@ -16,6 +16,23 @@ export function activate() {
 			markerManager.clearMarkers();
 			evt.abortKeyBinding();
 		});
+
+	// Insert formated line break between tags
+	atom.commands.add('atom-text-editor', 'emmet:insert-formatted-line-break', function(evt) {
+		const editor = this.getModel();
+
+		if (!editor.hasMultipleCursors()) {
+			// TODO support multiple cursors
+			const scope = editor.scopeDescriptorForBufferPosition(editor.getCursorBufferPosition());
+			for (let i = 0; i < scope.scopes.length; i++) {
+				if (scope.scopes[i].includes('.between-tag-pair.html')) {
+					return insertFormattedLineBreak(editor);
+				}
+			}
+		}
+
+		evt.abortKeyBinding();
+	});
 }
 
 export function deactivate() {
@@ -23,4 +40,23 @@ export function deactivate() {
 		markerManager.dispose();
 		markerManager = null;
 	}
+}
+
+function insertFormattedLineBreak(editor, cursor) {
+	cursor = cursor || editor.getLastCursor();
+	const startPos = cursor.selection.getBufferRange().start;
+
+	editor.transact(() => {
+		cursor.selection.insertText('\n\t\n', { autoIndent: false });
+		editor.normalizeTabsInBufferRange(cursor.selection.getBufferRange());
+
+		// Indent inserted text
+		const row = startPos.row;
+		const indent = editor.lineTextForBufferRow(row).match(/^\s*/)[0];
+		editor.buffer.insert([row + 1, 0], indent);
+		editor.buffer.insert([row + 2, 0], indent);
+
+		editor.moveUp();
+		editor.moveToEndOfLine();
+	});
 }
