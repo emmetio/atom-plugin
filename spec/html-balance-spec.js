0f50278f0cb9f582ff33679c50f4363a5a96049f
Sergey Chikuyonok <serge.che@gmail.com>
2017-03-28 12:13:23 +0300

    Minor specs refactoring

--- spec/html-balance-spec.js
+++ spec/html-balance-spec.js
@@ -8,6 +8,7 @@ describe('HTML Balance Action', () => {
 	let editor;
 	const filePath = path.resolve(__dirname, './fixtures/sample.html');
 	const runCommand = name => atom.commands.dispatch(atom.views.getView(editor), name);
+	const getSelection = () => editor.getSelectedBufferRange();
 
 	beforeEach(() => {
 		waitsForPromise(() => atom.packages.activatePackage(pkg.name));
@@ -21,60 +22,52 @@ describe('HTML Balance Action', () => {
 	});
 
 	it('should balance outward', () => {
+		const run = () => runCommand('emmet:balance-outward');
+
 		// Go to initial position
 		editor.setCursorBufferPosition([4, 9]);
 
-		// should match <meta> tag
-		runCommand('emmet:balance-outward');
-		expect(editor.getSelectedBufferRange()).toEqual(new Range([4, 1], [4, 71]));
+		run(); // should match <meta> tag
+		expect(getSelection()).toEqual(new Range([4, 1], [4, 71]));
 
-		// should match inner contents of <head> tag
-		runCommand('emmet:balance-outward');
-		expect(editor.getSelectedBufferRange()).toEqual(new Range([2, 6], [7, 0]));
+		run(); // should match inner contents of <head> tag
+		expect(getSelection()).toEqual(new Range([2, 6], [7, 0]));
 
-		// should match <head> tag
-		runCommand('emmet:balance-outward');
-		expect(editor.getSelectedBufferRange()).toEqual(new Range([2, 0], [7, 7]));
+		run(); // should match <head> tag
+		expect(getSelection()).toEqual(new Range([2, 0], [7, 7]));
 
-		// should match inner contents of <html> tag
-		runCommand('emmet:balance-outward');
-		expect(editor.getSelectedBufferRange()).toEqual(new Range([1, 16], [15, 0]));
+		run(); // should match inner contents of <html> tag
+		expect(getSelection()).toEqual(new Range([1, 16], [15, 0]));
 
-		// should match <html> tag
-		runCommand('emmet:balance-outward');
-		expect(editor.getSelectedBufferRange()).toEqual(new Range([1, 0], [15, 7]));
+		run(); // should match <html> tag
+		expect(getSelection()).toEqual(new Range([1, 0], [15, 7]));
 
-		// keep previous selection
-		runCommand('emmet:balance-outward');
-		expect(editor.getSelectedBufferRange()).toEqual(new Range([1, 0], [15, 7]));
+		run(); // keep previous selection
+		expect(getSelection()).toEqual(new Range([1, 0], [15, 7]));
 	});
 
 	it('should balance inward', () => {
+		const run = () => runCommand('emmet:balance-inward');
+
 		// Go to initial position
 		editor.setCursorBufferPosition([1, 4]);
 
-		// should match outward first
-		runCommand('emmet:balance-inward');
-		expect(editor.getSelectedBufferRange()).toEqual(new Range([1, 0], [15, 7]));
+		run(); // should match outward first
+		expect(getSelection()).toEqual(new Range([1, 0], [15, 7]));
 
-		// should match inner contents of <html> tag
-		runCommand('emmet:balance-inward');
-		expect(editor.getSelectedBufferRange()).toEqual(new Range([1, 16], [15, 0]));
+		run(); // should match inner contents of <html> tag
+		expect(getSelection()).toEqual(new Range([1, 16], [15, 0]));
 
-		// should match <head> tag
-		runCommand('emmet:balance-inward');
-		expect(editor.getSelectedBufferRange()).toEqual(new Range([2, 0], [7, 7]));
+		run(); // should match <head> tag
+		expect(getSelection()).toEqual(new Range([2, 0], [7, 7]));
 
-		// should match inner contents of <head> tag
-		runCommand('emmet:balance-inward');
-		expect(editor.getSelectedBufferRange()).toEqual(new Range([2, 6], [7, 0]));
+		run(); // should match inner contents of <head> tag
+		expect(getSelection()).toEqual(new Range([2, 6], [7, 0]));
 
-		// should match first <meta> tag
-		runCommand('emmet:balance-inward');
-		expect(editor.getSelectedBufferRange()).toEqual(new Range([3, 1], [3, 23]));
+		run(); // should match first <meta> tag
+		expect(getSelection()).toEqual(new Range([3, 1], [3, 23]));
 
-		// should keep selection
-		runCommand('emmet:balance-inward');
-		expect(editor.getSelectedBufferRange()).toEqual(new Range([3, 1], [3, 23]));
+		run(); // should keep selection
+		expect(getSelection()).toEqual(new Range([3, 1], [3, 23]));
 	});
 });
